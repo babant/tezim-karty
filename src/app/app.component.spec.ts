21021c17bf76d0cafe5d87ac77ed7538ce9ee94b
Ondra Hlaváč <ondra@hlavac.cz>
2018-06-18 22:23:46 +0200

    this is just an initialization of this repo, bro

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,25 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
+
+
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+
+
+
+  it(`should have as title 'tezim-karty'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('tezim-karty');
   }));
+
+
+
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
