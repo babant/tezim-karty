f27c4f7ac9a6a96f5fb00e1fb33c8320caea4660
Ondřej Hlaváč <ondrej.hlavac@korunka.eu>
2020-12-03 18:42:25 +0100

    Setting up new project on React and Gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    siteMetadata: {
+        title: `Tezim karty`,
+        description: `Simple credit card management web application.`,
+        author: `Ondra Hlaváč`,
+    },
+    plugins: [
+
+    ]
+}
